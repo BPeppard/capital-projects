a14ff0506a1930c5ff607db5905622e4588d69e9
BPeppard <>
2018-11-21 08:58:46 -0600

    new request page

--- pages/requests/index.js
+++ pages/requests/index.js
@@ -1,9 +1,17 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import Link from 'next/link';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
-import Page from "../../components/Page";
-import Layout from "../../components/Layout";
+import Page from '../../components/Page';
+import Layout from '../../components/Layout';
 
 const styles = theme => ({});
 
@@ -13,9 +21,32 @@ class Index extends Page {
 
     return (
       <Layout session={this.props.session}>
-        <Typography variant="h4" gutterBottom>
-          Requests
-        </Typography>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography variant="h4" gutterBottom>
+              Requests
+            </Typography>
+          </Grid>
+          <Grid item xs={6} align="right">
+            <Link href="/requests/new">
+              <Button variant="contained" color="primary">
+                New Request
+              </Button>
+            </Link>
+          </Grid>
+        </Grid>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Department</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Priority</TableCell>
+              <TableCell>Cost</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
       </Layout>
     );
   }
